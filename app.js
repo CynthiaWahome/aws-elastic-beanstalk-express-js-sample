5d2d91627d3442eded85c51f9d497da227bc8007
Cynthia <160864573+CynthiaWahome@users.noreply.github.com>
2024-07-07 14:49:27 +0300

    Update app.js
    change typo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!This is my the creation of my first Continuous Delivery Pipeline'));
+app.get('/', (req, res) => res.send('Hello World!This is the creation of my first Continuous Delivery Pipeline'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
